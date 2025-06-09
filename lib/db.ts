8a7351a6fc35f06fbb0a0d030525a7f65fa74072
Aryan0512398 <aryangupta052015@gmail.com>
2025-06-09 19:50:38 +0530

    Deployment Changes

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-// Prevent multiple Prisma Client instances in development
-// eslint-disable-next-line no-var
+// add no-var
+/* eslint-disable no-var */
 declare global {
   var prisma: PrismaClient | undefined;}
 
+  /* eslint-disable no-var */
 export const db=globalThis.prisma ||  new PrismaClient();
 if(process.env.NODE_ENV !== "production") {
   globalThis.prisma = db;
